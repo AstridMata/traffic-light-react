91c36aae327cf30cbf6df61644607ecbcdbca5ca
JoaquinAlzugaray <joaquinalzugaray94@gmail.com>
2022-01-27 02:35:09 +0000

    buttons

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,20 +6,31 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	const [color, setColor] = useState("btn-primary");
+	// const [color, setColor] = useState("btn-primary");
 
-	function changeColor() {
-		color === "btn-primary"
-			? setColor("btn-danger")
-			: setColor("btn-primary");
-	}
+	// function changeColor() {
+	// 	color === "btn-primary"
+	// 		? setColor("btn-danger")
+	// 		: setColor("btn-primary");
+	// }
+	// <button className={"btn " + color} onClick={changeColor}>
+	// 	Button
+	// </button>
+
 	return (
 		<div>
-			<button className={"btn " + color} onClick={changeColor}>
-				Button
-			</button>
-			<button className="btn btn-success">Button</button>
-			<button className="btn btn-danger">Button</button>
+			<div id="trafficTop"></div>
+			<div className="container">
+				<div className="red light">
+					<button className="btn btn-danger"></button>
+				</div>
+				<div className="yellow light">
+					<button className="btn btn-warning"></button>
+				</div>
+				<div className="green light">
+					<button className="btn btn-success"></button>
+				</div>
+			</div>
 		</div>
 	);
 };
